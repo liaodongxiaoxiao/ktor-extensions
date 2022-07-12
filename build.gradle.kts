5ef1e16ff34a8c48536b71c71a95ae00af9ae47b
wangzhuo <wangzhuo@autoai.com>
2022-07-12 16:43:17 +0800

    update gradle for jitpack io

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.api.publish.PublishingExtension
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -6,6 +7,7 @@ val exposed_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.7.0"
+    id("maven-publish")
 }
 
 group = "com.karl.coder"
@@ -20,6 +22,7 @@ application {
 repositories {
     mavenCentral()
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
@@ -32,8 +35,22 @@ dependencies {
     implementation("io.ktor:ktor-serialization-gson-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
 
-    implementation  ("mysql:mysql-connector-java:8.0.29")
-    implementation ("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation ("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation ("org.jetbrains.exposed:exposed-jodatime:$exposed_version")
+    implementation("mysql:mysql-connector-java:8.0.29")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jodatime:$exposed_version")
+}
+
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.liaodongxiaoxiao"
+            artifactId = "ktor-extensions"
+            version = "v1.0.1"
+
+            from(components["java"])
+        }
+    }
 }
